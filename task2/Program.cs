e35a0085f2bfaa03cb8ef4794e4841b5bca6f91b
Egor Pavlovskiy <pavlovskiyev0610@gmail.com>
2024-03-20 17:46:02 +0300

    hw8

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+int AckermanFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return AckermanFunc(m - 1, 1);
+    }
+    else
+    {
+        return AckermanFunc(m - 1, AckermanFunc(m, n - 1));
+    }
+}
+int m = 2;
+int n = 3;
+int result = AckermanFunc(m, n);
+Console.Write($"A({m}, {n}) = {result}");
---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+void PrintReversedArray (int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(array[index] + " ");
+        PrintReversedArray(array, index - 1);
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6 };
+PrintReversedArray(array, array.Length - 1);
+
